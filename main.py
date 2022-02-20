fb16284bcb4d544df3a2cefe7c74fc6ebc0c8775
Gabriel Hernandes <ghh.hernandes@gmail.com>
2022-02-20 12:05:44 -0300

    main.py change

--- main.py
+++ main.py
@@ -1,18 +1,12 @@
 from scrapping import matches, schemas
 
-def print_match(match: schemas.Match) -> None:
-    print(f"\t({match.info.time}) {match.event.name}")
-    print(f"\t\t{match.teams[0].name}")
-    print(f"\t\t{match.teams[1].name}")
-
 if __name__ == '__main__':
-    match_service = matches.MatchService("https://www.hltv.org/matches/")   
-    
+    match_service = matches.MatchService("https://www.hltv.org/matches/") 
+
     print('Live CS:GO matches')
     for match in match_service.get_live_matches():
-        print_match(match)
+        print(match)
 
     print('\nUpcoming CS:GO matches')
     for match in match_service.get_upcoming_matches():
-        print_match(match)
-    
+        print(match)
